69cca014e2a53bcd1dfc8a1a2b70b9008f390d41
David Shaw <david.shaw@skybettingandgaming.com>
2019-04-19 19:55:08 +0100

    process.browser hack

--- src/utils/identity.js
+++ src/utils/identity.js
@@ -1,24 +1,28 @@
 import { useState, useEffect } from 'react';
 import netlifyIdentity from 'netlify-identity-widget';
-
-
 // You must run this once before trying to interact with the widget
-netlifyIdentity.init();
+if (process.browser) { 
+  netlifyIdentity.init();
+}
 
 const useIdentity = () => {
-  const [user, setUser] = useState(null);
-  useEffect(()=> {
-    const user = netlifyIdentity.currentUser();
-    if ( user ){
-      setUser(user);
-    } else {
-      netlifyIdentity.open();
-      netlifyIdentity.on('login', user => {
+  if (process.browser) {
+    const [user, setUser] = useState(null);
+    useEffect(()=> {
+      const user = netlifyIdentity.currentUser();
+      if ( user ){
         setUser(user);
-      });
-    }
-  });
-  return user;
+      } else {
+        netlifyIdentity.open();
+        netlifyIdentity.on('login', user => {
+          setUser(user);
+        });
+      }
+    });
+    return user;
+  } else {
+    return null;  
+  }
 }
 
 export default useIdentity;
